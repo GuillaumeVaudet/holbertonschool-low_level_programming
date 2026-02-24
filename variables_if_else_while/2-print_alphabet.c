3de351dc3a1666d671328f52e422d86a9e54c3d2
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-24 15:00:26 +0100

    Add print alphabet script

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	int c;
+
+	c = 97;
+
+	while (c <= 122)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
