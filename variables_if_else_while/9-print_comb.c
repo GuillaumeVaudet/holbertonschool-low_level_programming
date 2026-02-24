9f0c9274a9c174173a8f6db45edc6df95cf22b41
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-24 23:19:30 +0100

    Add print comb script

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+
+	/* Expected Output Result 0, 1, 2, 3, 4, 5, 6, 7, 8, 9*/
+	int n;
+	n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
