3ffad3f9bf213f7183b7337b3694209d7855d9a2
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-24 13:41:59 +0100

    Add last digit script

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %i and is greater than 5", n, last_digit);
+	}
+	if (last_digit == 0)
+	{
+		printf("Last digit of %d is %i and is 0", n, last_digit);
+	}
+	if (last_digit < 6)
+	{
+		printf("Last digit of %d is %i and is less than 6 and not 0", n, last_digit);
+	}
+	return (0);
+}
