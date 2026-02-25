a4ca830a1640de77e1b83a5dda4ac06444fb4c34
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-25 12:36:27 +0100

    Add print alphabet x 10 script

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Print alphabet ten time in lowercase
+* using _putchar
+*
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int i;
+
+	a = 97;
+	i = 0;
+
+	while (i < 10)
+	{
+		while (a <= 122)
+		{
+			_putchar(a);
+			a++;
+		}
+		a = 97;
+		i++;
+		_putchar('\n');
+	}
+
+}
---
+++ functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*   main - Entry Point
+*
+*   Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
