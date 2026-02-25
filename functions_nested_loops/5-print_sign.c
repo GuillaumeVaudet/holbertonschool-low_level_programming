a02a747f209e35fe59e37c7e814c52a906b8b1d8
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-25 13:45:27 +0100

    Add print sign script

---
+++ functions_nested_loops/5-print_sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_sign - prints the sign of a number
+* @n: The int to test
+* Return: 1 and prints '+' if n is positive and 0 if n == 0,
+* -1 and prints '-' if n is negative
+*/
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	} else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	_putchar('0');
+	return (0);
+}
