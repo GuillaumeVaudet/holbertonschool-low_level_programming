33b1aac3a43caa9afe8d796dc9991a9274ddbaa4
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-25 12:13:16 +0100

    Add print alphabet script

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	int a;
+
+	a = 97;
+
+	while (a <= 122)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
