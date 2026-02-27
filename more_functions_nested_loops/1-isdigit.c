39c02ac36fb5a15c0b6ddde467dca34fa1302db3
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-27 23:54:01 +0100

    Add is digit script

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+/**
+* _isdigie - check if a char is upper or not
+* @c: item to test
+* Return: 1 if c is updigitper and 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
