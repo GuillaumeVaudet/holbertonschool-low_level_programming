4334a903a7e20ae9517211ee608f454bc6cef83a
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-27 11:30:37 +0100

    Add 10-add script

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - adds two integers and returns the result
+* @a: First parameter to make addition
+* @b: Second parameter to make addition
+* Return: the result of addition
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
