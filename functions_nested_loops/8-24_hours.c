ecbcf7021922a64225e984534c40902017dc276e
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-27 10:11:18 +0100

    Fix miss copied section

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -17,16 +17,14 @@ void jack_bauer(void)
 	while (hours < 24)
 	{
 		while (minutes < 60)
-		{		_putchar((hours / 10) + '0');
-		_putchar((hours % 10) + '0');
-		_putchar(':');
-		_putchar((hours / 10) + '0');
-		_putchar((hours % 10) + '0');
-		_putchar(':');
-		_putchar((minutes / 10) + '0');
-		_putchar((minutes % 10) + '0');
-		_putchar('\n');
-		minutes++;
+		{		
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+			minutes++;
 		}
 	minutes = 0;
 	hours++;
