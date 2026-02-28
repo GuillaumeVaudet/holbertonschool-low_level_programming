aadddff44c57db213161bba3d5edfcaa7b846d3c
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-28 23:33:43 +0100

    Fix number of putchar

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,21 @@
 
 void more_numbers(void)
 {
-		int i;
+    int i;
+    int j;
 
-		i = 0;
-
-		while (i <= 14)
-		{
-			if (i <= 9)
-			{
-				_putchar(i + '0');
-			} else
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			i++;
-		}
-		_putchar('\n');
+    j = 0;
+    while (j < 10)
+    {
+        i = 0;
+        while (i <= 14)
+        {
+            if (i >= 10)
+                _putchar(i / 10 + '0');
+            _putchar(i % 10 + '0');
+            i++;
+        }
+        _putchar('\n');
+        j++;
+    }
 }
