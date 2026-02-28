7473d2bce9534e28be05f50c1c8f873db4024fe0
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-28 14:00:28 +0100

    Add more numbers script need to lint

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line.
+*
+* Return: void
+*/
+
+void more_numbers(void)
+{
+		int i;
+
+		i = 0;
+
+		while (i <= 14)
+		{
+				if (i <= 9)
+				{
+					_putchar(i + '0');
+				} else
+				{
+					_putchar(i / 10 + '0');
+					_putchar(i % 10 + '0');
+				}
+			i++;
+		}
+		_putchar('\n');
+}
