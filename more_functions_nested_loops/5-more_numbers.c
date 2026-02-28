882b49b6e33dd1ea2795550bd08b575f3ae43b1b
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-02-28 23:23:37 +0100

    Fix betty style for the fifth script

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-* more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line.
-*
+* more_numbers - prints 10 times the numbers, from 0 to 14
 * Return: void
 */
 
@@ -13,14 +12,14 @@ void more_numbers(void)
 
 		while (i <= 14)
 		{
-				if (i <= 9)
-				{
-					_putchar(i + '0');
-				} else
-				{
-					_putchar(i / 10 + '0');
-					_putchar(i % 10 + '0');
-				}
+			if (i <= 9)
+			{
+				_putchar(i + '0');
+			} else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
 			i++;
 		}
 		_putchar('\n');
