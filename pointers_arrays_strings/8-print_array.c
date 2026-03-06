041925ea7d664ddb00a627c525d4c9f456eed59f
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-03-06 18:44:51 +0100

    Add print array script

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* print_array - prints n elements of an array of int
+* @a: pointer
+* @n: int
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		printf("%i, ", a[i]);
+		i++;
+	}
+	printf("\n");
+}
