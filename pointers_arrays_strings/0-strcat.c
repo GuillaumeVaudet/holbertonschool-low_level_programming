a3def7797ff6fe5ba1e05eaa7b4562f2b6ace658
guillaume vaudet <guillaumevaudet.pro@gmail.com>
2026-03-08 19:44:28 +0100

    Add strcat script

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+/**
+* _strcat - concatenates two strings
+* @dest: destination string
+* @src: src string
+* Return: char pointer
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	return (dest);
+}
