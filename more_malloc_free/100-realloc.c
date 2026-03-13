c5005b4d6d03a83ea8c815640728a6724fb4a76d
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-03-13 16:17:16 +0100

    Fix the condition where new size is less than old size

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 /**
-* realloc - reallocates a memory block
+* _realloc - reallocates a memory block
 * @ptr: poointer where the old data is stored
 * @old_size: old size of memory allocation
 * @new_size: new memory size to allocate
@@ -10,12 +10,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr;
 	unsigned int i = 0;
+	unsigned int copy_size;
+
+	if (old_size < new_size)
+	{
+		copy_size = old_size;
+	} else
+		copy_size = new_size;
 
 	if (new_size == old_size)
 		return (ptr);
 
-	if (ptr == NULL && new_size == 0)
+	if (new_size == 0)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
 	if (ptr == NULL)
 	{
@@ -28,7 +38,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-	while (i < old_size)
+	while (i < copy_size)
 	{
 		new_ptr[i] = ((char *)ptr)[i];
 		i++;
