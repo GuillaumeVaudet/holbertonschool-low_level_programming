acdb11322f55191f5d7c5c775938d764d95e98d7
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-03-16 15:04:07 +0100

    Add structures typedef dir with readme and first header

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+# This is the directory to push all exercices about structs and typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - define a dog object
+* @name: string - used for the dog's name
+* @age: integer - used for dog's age
+* @owner: string - used for the dog's owner name
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
