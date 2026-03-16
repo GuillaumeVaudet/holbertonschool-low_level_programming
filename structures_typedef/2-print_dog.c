1b6daf54e6e189cf86f5196c681248580e507f23
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-03-16 21:14:15 +0100

    Add print dog script

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+* print_dog - print a struct called dog
+* @d: pointer to a type struct dog
+* Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
