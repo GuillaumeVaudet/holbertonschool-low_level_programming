af243dfb8ef678fe5b868b69dc08fe951d0e651b
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-03-19 21:44:59 +0100

    Add the NULL case for name and f

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
 * print_name - prints a name from a function pointer
 * @name: pointer to a string
@@ -7,5 +8,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
