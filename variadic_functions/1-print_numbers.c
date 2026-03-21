3dfa74c33ae008612ce2706ce240bfb083d211d2
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-03-21 09:43:20 +0100

    Fix condition to print separator into print number et fix guard name into header

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,15 +12,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
-
 	i = 0;
 	va_start(args, n);
 	while (i < n)
 	{
 		printf("%d", va_arg(args, int));
-		if (i < n - 1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 		i++;
 	}
