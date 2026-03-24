8902d1d309d9478665083bc97e34fe8fc7f50570
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-03-24 21:00:50 +0100

    Add strlen recursion script

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+/**
+* _strlen_recursion - return the lenght of a string
+* @s: pointer to string
+* Return: int
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
