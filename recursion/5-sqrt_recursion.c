1b728db03acd1c4ae998778d3f884db2dce6e1c5
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-03-29 22:20:26 +0200

    Add sqrt recursion script

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,9 +1,26 @@
 /**
+ * square_helper - checks for the square root
+ * @n: int
+ * @i: int
+ * Return: int
+ */
+int square_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (square_helper(n, i + 1));
+}
+
+/**
 * _sqrt_recursion - found the natural square of a number
 * @n: int
 * Return: int
 */
 int _sqrt_recursion(int n)
 {
-
+	if (n < 0)
+		return (-1);
+	return (square_helper(n, 0));
 }
