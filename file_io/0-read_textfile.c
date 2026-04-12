b5c50c757736d500d9f494eec2e37d1c7d4b20dd
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-04-12 12:32:44 +0200

    Add read_textfile script with proto into main.h

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdlib.h>
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: pointer to string - path of the filename
+ * @letters: size_t - number of letters it should read and print
+ * Return: ssize_t - the actual number of letters it could read and print
+ * 0 if the file can not be opened or read - if filename is NULL
+ * or if write fails or does not wirte the expected amount of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	bytes_written = write(1, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (bytes_written);
+}
